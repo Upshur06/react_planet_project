66fd0bc2f7c57c7ad3036a7922b580f82b87c7e8
Upshur06 <mupshur2005@gmail.com>
2021-09-09 01:17:07 -0400

    Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
+
+export default function Header(){
+    return (
+        <div className='header'>
+            <h2>The Planets</h2>
+            <div className="nav">
+                <Link className='nav__item' to='/'>mercury</Link>
+                <Link className='nav__item' to='/venus'>venus</Link>
+                <Link className='nav__item' to='/earth'>earth</Link>
+                <Link className='nav__item' to='/mars'>mars</Link>
+                <Link className='nav__item' to='/jupiter'>jupiter</Link>
+                <Link className='nav__item' to='/saturn'>saturn</Link>
+                <Link className='nav__item' to='/uranus'>uranus</Link>
+                <Link className='nav__item' to='/neptune'>neptune</Link>
+            </div>
+        </div>
+    )
+}
